dbb67342c1aa7de2abe6a89b3abc3e75e86e89a4
bendangnuksung <bendangnuksungimsong@gmail.com>
2022-01-24 11:01:52 +0530

    fix bugs

--- drawline/utils.py
+++ drawline/utils.py
@@ -119,13 +119,46 @@ def sort_contours_by_area(contours, labels):
     return sorted_contours, sorted_labels
 
 
+# https://github.com/PyImageSearch/imutils/blob/master/imutils/convenience.py
+def grab_contours(cnts):
+    """ TBD """
+    # if the length the contours tuple returned by cv2.findContours
+    # is '2' then we are using either OpenCV v2.4, v4-beta, or
+    # v4-official
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+    # if the length of the contours tuple is '3' then we are using
+    # either OpenCV v3, v4-pre, or v4-alpha
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    # otherwise OpenCV has changed their cv2.findContours return
+    # signature yet again and I have no idea WTH is going on
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+                         "otherwise OpenCV changed their cv2.findContours return "
+                         "signature yet again. Refer to OpenCV's documentation "
+                         "in that case"))
+
+    # return the actual contours array
+    return cnts
+
+
+def get_contour_from_mask(mask):
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contour = grab_contours(cnts)
+    contour = np.squeeze(contour)
+    return contour
+
+
 def prepare_val_contours(contours):
-    if type(contours) in [list, tuple]:
+    if isinstance(contours, (list, tuple)):
         contours = np.array(contours)
 
     new_contours = []
 
-    if len(contours.shape) == 1:
+    if len(contours.shape) == 1 or len(contours.shape) == 3:
         for contour in contours:
             contour = np.squeeze(contour)
             new_contours.append(contour)
