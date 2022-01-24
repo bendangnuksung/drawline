297c95bd4433ac46ceae38dceeb3a8290f5d16a4
bendangnuksung <bendangnuksungimsong@gmail.com>
2022-01-24 11:06:22 +0530

    update to 0.3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='drawline',
-    version='0.3.1',
+    version='0.3.2',
     packages=['drawline'],
     url='https://github.com/bendangnuksung/drawline',
     license='MIT License',
